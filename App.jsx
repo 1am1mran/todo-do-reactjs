507c735ee6426bbe27515bf969a7bd804bc69ed3
1am1mran <imran.abdul.186@gmail.com>
2018-01-12 15:10:01 +0530

    AppList.jsx modified for deleting a task

--- App.jsx
+++ App.jsx
@@ -8,10 +8,16 @@ class App extends React.Component {
         super();
         this.state = {tasks: props.tasks};
         this.updateList = this.updateList.bind(this);
+        this.removeTask = this.removeTask.bind(this);
     }
     updateList(text)    {
         var updatedTasks = this.state.tasks;
         updatedTasks.push(text);
+        this.setState({tasks: updatedTasks});
+    }
+    removeTask(text){
+        var updatedTasks = this.state.tasks;
+        updatedTasks.splice(updatedTasks.indexOf(text), 1);
         this.setState({tasks: updatedTasks});
     }
     render() {
@@ -19,7 +25,7 @@ class App extends React.Component {
             <div>
                 <h1>Todo App</h1>
                 <AddNewTask updateList={this.updateList}/>
-                <AppList tasks={this.state.tasks} />
+                <AppList tasks={this.state.tasks} remove={this.removeTask}/>
             </div>
         );
     }
--- AppList.jsx
+++ AppList.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 
 class AppList extends React.Component {
-    constructor(){
+    constructor() {
         super();
+        this.remove = this.remove.bind(this);
+    }
+
+    remove(elem) {
+        var value = elem.target.parentNode.querySelector('span').innerText;
+        this.props.remove(value);
     }
     render() {
-        var items = this.props.tasks.map((elem, i) =>{
-            return <li key={i}>{elem}</li>
+        var items = this.props.tasks.map((elem, i) => {
+            return <li key={i}>
+                <span>{elem}</span>
+                <button onClick={this.remove}>X</button>
+            </li>
         })
         return (
             <ul>
